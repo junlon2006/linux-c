5404bb14ba1d0c9b4bcd4da70852778b79af7813
shangjinlong <shangjinlong@unisound.com>
2019-12-15 21:26:51 +0800

    :construction: Float to String

--- Float2String.c
+++ Float2String.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 /** Number on countu **/
 static int __n_tu(int number, int count) {
@@ -53,13 +55,23 @@ static void __float_to_string(float f, char r[]) {
 
 /* Thread unsafe 线程不安全函数，有效精度8位（包含整数和小数部分），超过不能保证准确性 */
 const char* float_to_string(float f) {
+    f += 0.00001;
     static char floatBuf[128];
     __float_to_string(f, floatBuf);
     return floatBuf;
 }
 
-int main() {
-  printf("%s\n", float_to_string(-0.23456));
-  printf("%s\n", float_to_string(-1111.12345678));
-  return 0;
+#define EPSION      (0.001)
+int main(int argc, char *argv[]) {
+    float f = -100;
+    int count = 0;
+    while (f <= 100) {
+        float nf = strtof(float_to_string(f += EPSION), NULL);
+        if (fabsf(nf - f) > 0.002) {
+            printf("ERROR: f=%f, nf=%f\n", f, nf);
+        }
+        count++;
+    }
+    printf("count=%d\n", count);
+    return 0;
 }
