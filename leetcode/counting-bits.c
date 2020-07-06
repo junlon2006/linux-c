9459846fe99e6db2066014dc277024397381daae
shangjinlong <shangjinlong@unisound.com>
2020-07-06 16:06:22 +0800

    counting-bits

--- leetcode/counting-bits.c
+++ leetcode/counting-bits.c
@@ -32,3 +32,13 @@ int* countBits(int num, int* returnSize){
     }
     return result;
 }
+
+int* countBits(int num, int* returnSize){
+    *returnSize = num + 1;
+    int *result = malloc(sizeof(int) * (num + 1));
+    result[0] = 0;
+    for (int i = 1; i <= num; i++) {
+        result[i] = result[i >> 1] + (i & 1);
+    }
+    return result;
+}
