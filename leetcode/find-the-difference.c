35065e59dbbf22aa8faafa8843e669f161d9e2f9
shangjinlong <shangjinlong@unisound.com>
2020-08-13 18:47:49 +0800

    find-the-difference

--- leetcode/find-the-difference.c
+++ leetcode/find-the-difference.c
@@ -38,3 +38,10 @@ char findTheDifference(char * s, char * t){
     }
     return 'a';
 }
+
+char findTheDifference(char * s, char * t){
+    int result = 0;
+    while (*s) result ^= *s++;
+    while (*t) result ^= *t++;
+    return (char)result;
+}
